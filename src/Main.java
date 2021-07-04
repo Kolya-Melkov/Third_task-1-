6a8c24cf78f9555a39d9dda2a078f71b994a3931
Nikolay Melkov <pestrrroy@gmail.com>
2021-07-04 12:54:53 +0300

    Скидка на авиабилеты(3.1)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 231.8;
+        int cutPrice = 20;
+        int bonus = (int)(ticketPrice / cutPrice);
+        System.out.println(bonus);
+    }
+}
